1a8eb8d6ec383e4599e9a4af279c4789352841af
Harianto Rais <ibnurais.abdillah@gmail.com>
2026-03-02 00:14:33 +0700

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useEffect } from "react";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 
 function App() {
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users",
-        );
-
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-
-    fetchUsers();
-  }, []);
-  return <div></div>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import AuthContext from "../AuthContext";
 
 export default function Navbar() {
-  const { user, logout } = useContext(AuthContext);
   return (
-    <nav style={{ flexDirection: "row", display: "flex", gap: "1rem" }}>
-      <div style={{ gap: "1rem", display: "flex" }}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <div style={{ gap: "1rem", display: "flex", marginLeft: "auto" }}>
-        {!user.isAuth ? (
-          <Link to="/login">Login</Link>
-        ) : (
-          <button onClick={logout}>Logout</button>
-        )}
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-brand">
+          ShopHub
+        </Link>
+        <div className="navbar-links">
+          <Link to="/">Home</Link>
+          <Link to="/checkout">Cart</Link>
+        </div>
+        <div className="navbar-auth">
+          <div className="navbar-auth-links">
+            <Link to="/auth" className="btn btn-secondary">
+              Login
+            </Link>
+            <Link to="/auth" className="btn btn-primary">
+              Signup
+            </Link>
+          </div>
+        </div>
       </div>
     </nav>
   );

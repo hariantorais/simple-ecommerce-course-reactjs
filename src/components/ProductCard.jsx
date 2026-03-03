cbf82f4fee9826cb6d384f1a443de8eea4dcb9a9
Harianto Rais <ibnurais.abdillah@gmail.com>
2026-03-04 00:15:01 +0700

    finished cart and checkout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
 
 export default function ProductCard({ product }) {
+  const { addToCart, cartItems } = useCart();
+
+  const productInCart = cartItems.find((item) => item.id === product.id);
+
+  const productQuantityLabel = productInCart
+    ? `(${productInCart.quantity})`
+    : "";
   return (
     <div className="product-card" key={product.id}>
       <img src={product.image} alt={product.name} className="product-image" />
@@ -11,7 +19,12 @@ export default function ProductCard({ product }) {
           <Link to={`/products/${product.id}`} className="btn btn-secondary">
             View Details
           </Link>
-          <button className="btn btn-primary">Add to Cart</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => addToCart(product.id)}
+          >
+            Add to Cart {productQuantityLabel}
+          </button>
         </div>
       </div>
     </div>
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getProductById } from "../data/products";
+import { useCart } from "../context/CartContext";
 
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
+
+  const { addToCart, cartItems } = useCart();
 
   const navigate = useNavigate();
 
@@ -21,6 +24,12 @@ export default function ProductDetails() {
     return <div>Loading...</div>;
   }
 
+  const productInCart = cartItems.find((item) => item.id === product.id);
+
+  const productQuantityLabel = productInCart
+    ? `(${productInCart.quantity})`
+    : "";
+
   return (
     <div className="page">
       <div className="container">
@@ -32,7 +41,12 @@ export default function ProductDetails() {
             <h1>{product.name}</h1>
             <p className="product-detail-price">${product.price}</p>
             <p className="product-detail-description">{product.description}</p>
-            <button className="btn btn-primary">Add to Cart</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => addToCart(product.id)}
+            >
+              Add to Cart {productQuantityLabel}
+            </button>
           </div>
         </div>
       </div>
